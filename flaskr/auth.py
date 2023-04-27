f7335e74f7ff1d93cb6a097623a000478084f5cb
Orlando <rodriguezoliverao@moravian.edu>
2023-04-27 09:33:44 -0400

    tried to redo buncha files

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,16 +1,13 @@
 import functools
 
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, 
-url_for
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
-
-
 
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
@@ -40,7 +37,6 @@ def register():
         flash(error)
 
     return render_template('auth/register.html')
-
 
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
